63f7519609721be0d2f0477e9fed21360238fbb9
jennis-jpg <jenniswartika@gmail.com>
2026-08-02 07:39:57 +0530

    Delete seasentry_final_frontend/config/__init__.py

--- seasentry_final_frontend/config/__init__.py
+++
@@ -1,5 +0,0 @@
-"""SeaSentry frontend configuration."""
-
-from config.theme import COLORS, DEMO_VESSEL, RISK_STYLES, inject_theme_css
-
-__all__ = ["COLORS", "DEMO_VESSEL", "RISK_STYLES", "inject_theme_css"]
